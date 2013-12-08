3f481f9142ff2c8fc3616ed9dc60d367e0af8270
elyas-bhy <elyas.bhy@gmail.com>
2013-12-08 19:28:05 +0100

    Added Maven and GAE configuration.

---
+++ source/freya-app/src/main/java/com/dev/freya/FreyaServlet.java
@@ -0,0 +1,19 @@
+package com.dev.freya;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FreyaServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+
+    resp.setContentType("text/plain");
+    resp.getWriter().println("Hello World!");
+  }
+  
+}
