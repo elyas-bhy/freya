4885ee07b7739a0512d11d9f04fe2bc075b58fe0
Largaroth <rya.herb@gmail.com>
2013-12-29 19:55:13 +0100

    added Artwork related endpoints for reproduction listing

--- source/freya-app/src/main/java/com/dev/freya/spi/v1/ArtworkEndpoints.java
+++ source/freya-app/src/main/java/com/dev/freya/spi/v1/ArtworkEndpoints.java
@@ -7,6 +7,7 @@ import javax.inject.Named;
 import com.dev.freya.dao.FreyaDao;
 import com.dev.freya.model.Artwork;
 import com.dev.freya.model.Photo;
+import com.dev.freya.model.Reproduction;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiMethod.HttpMethod;
@@ -70,5 +71,17 @@ public class ArtworkEndpoints {
 		List<Photo> photos = dao.getPhotosByArtwork(artworkId);
 		dao.close();
 		return photos;
+	}
+	
+	@ApiMethod(
+			name = "artworks.artwork.reproductions",
+			path = "artworks/{artwork_id}/reproductions",
+			httpMethod = HttpMethod.GET
+	)
+	public List<Reproduction> getReproductionsByArtwork(@Named("artwork_id") String artworkId) {
+		FreyaDao dao = new FreyaDao();
+		List<Reproduction> reproductions = dao.getReproductionsByArtwork(artworkId);
+		dao.close();
+		return reproductions;
 	}
 }
